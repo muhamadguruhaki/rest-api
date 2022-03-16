457db1ba8e865fba8975439e412f66fa3ecdaaa7
muhamadguruh15 <muhamadguruh15@gmail.com>
2022-03-16 15:12:35 +0700

    mengomentari array buatan dan membuat code PDO yg menghubungkan ke server local database latihan_phpmvc

--- latihan1.php
+++ latihan1.php
@@ -1,17 +1,22 @@
 <?php
 
-$mahasiswa = [
-    [
-        "nama" => "Muhamad Guruh",
-        "nim" => "18810081",
-        "email" => "muhamadguruh15@gmail.com"
-    ],
-    [
-        "nama" => "Naufal Fallah",
-        "nim" => "18810083",
-        "email" => "naufal66@gmail.com"
-    ]
-];
+// $mahasiswa = [
+//     [
+//         "nama" => "Muhamad Guruh",
+//         "nim" => "18810081",
+//         "email" => "muhamadguruh15@gmail.com"
+//     ],
+//     [
+//         "nama" => "Naufal Fallah",
+//         "nim" => "18810083",
+//         "email" => "naufal66@gmail.com"
+//     ]
+// ];
+
+$dbh = new PDO('mysql:host=localhost;dbname=latihan_phpmvc', 'root', '');
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
 
 $coba = json_encode($mahasiswa);
 echo $coba;
